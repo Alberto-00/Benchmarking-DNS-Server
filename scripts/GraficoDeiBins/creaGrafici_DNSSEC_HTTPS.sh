28c6b060172a92d34d073afd475b6790bb7a781c
GagliardeNicolapio <47244898+GagliardeNicolapio@users.noreply.github.com>
2024-01-21 18:32:32 +0100

    Update and rename creaGrafici.sh to creaGrafici_DNSSEC_HTTPS.sh

--- scripts/GraficoDeiBins/creaGrafici_DNSSEC_HTTPS.sh
+++ scripts/GraficoDeiBins/creaGrafici_DNSSEC_HTTPS.sh
@@ -5,4 +5,4 @@ sed 's/Bin [0-9]*: Range [.0-9]* - [.0-9]* ms, Richieste://g' $1 | tr '\n' ',' |
 echo -n '"' >> latenze_$1.txt
 sed 's/Bin [0-9]*: Range //g' $1 | sed 's/ ms, Richieste: [0-9]*//g' | tr '\n' 'A' | sed 's/A/","/g'  >> latenze_$1.txt
 
-python3 plotG.py answercount_$1.txt latenze_$1.txt $2 $3
+python3 plotG_DNSSEC_HTTPS.py answercount_$1.txt latenze_$1.txt $2 $3
