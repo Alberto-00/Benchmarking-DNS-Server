8e78b4c0eed68b0e3a063c1e131321d2cc342031
GagliardeNicolapio <47244898+GagliardeNicolapio@users.noreply.github.com>
2024-01-21 16:56:32 +0100

    Update create_plot.sh

--- scripts/GraficoDeiBins/create_plot.sh
+++ scripts/GraficoDeiBins/create_plot.sh
@@ -8,4 +8,4 @@ sed 's/  [.0-9]* - [.0-9]*:  /''/g' buckets_$1.txt | sed 's/Latency bucket (s): 
 
 sed 's/Latency bucket (s):   answer count/''/g' buckets_$1.txt | sed 's/Connection Statistics:/''/g' | sed 's/Reconnections:/''/g' | sed 's/[0-9]* ([.0-9]*% of [0-9]* connections)/''/g' | sed 's/Average Latency (s):  [.0-9]* (min [.0-9]*, max [.0-9]*)/''/g' | sed 's/:  [0-9]*/''/g' | sed 's/  /''/g'  | tr "\n" 'A' | sed 's/A/","/g'  | sed 's/"",/''/g' >> latenze_$1.txt
 
-python3 test333.py answer_count_$1.txt latenze_$1.txt $2 $3
+python3 plot.py answer_count_$1.txt latenze_$1.txt $2 $3
