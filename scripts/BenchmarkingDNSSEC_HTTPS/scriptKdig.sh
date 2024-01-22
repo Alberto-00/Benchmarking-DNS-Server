48e08a7b9a9c310fea3dfbbcf1271a116b59d994
GagliardeNicolapio <47244898+GagliardeNicolapio@users.noreply.github.com>
2024-01-22 12:26:20 +0100

    Update and rename httpsPerfParallel.sh to scriptKdig.sh

--- scripts/BenchmarkingDNSSEC_HTTPS/scriptKdig.sh
+++ scripts/BenchmarkingDNSSEC_HTTPS/scriptKdig.sh
@@ -4,7 +4,7 @@ calc(){ awk "BEGIN { print $* }"; }
 for line in `cat $1 | sed 's/ A//g'`
 do
 
-./httpsPerf.sh $line &
+./dnssec_httpsPerf.sh $line &
 if [ $(jobs | wc -l) == 100 ]
 then 
     sleep 1 
